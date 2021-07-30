31fbcd69f5a4d0a9852806c5b2fb7c51cea5a7cd
AdamMorytko <adikmor@gmail.com>
2021-07-30 15:05:58 +0200

    first commit

---
+++ src/main/java/pl/morytko/UsersCrudRestApplication.java
@@ -0,0 +1,13 @@
+package pl.morytko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersCrudRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersCrudRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/morytko/UsersCrudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.morytko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersCrudRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
